27e4a02b8906c6f27536a1867c542e1b0a4639c4
Mayank Kumar <mpareek365@gmail.com>
2022-11-03 12:53:50 -0400

    Create solution Created function to check if a year is leap or not by using modulo operator.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let isLeap = false;
+  if (year % 100 === 0) {
+    if (year % 400 === 0) isLeap = true;
+  } else if (year % 4 === 0) {
+    isLeap = true;
+  }
+  return isLeap;
 };
 
 // Do not edit below this line
