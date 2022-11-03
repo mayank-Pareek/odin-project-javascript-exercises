bd83c1d3686a85edd561de73e58cb8554d4bd098
Mayank Kumar <mpareek365@gmail.com>
2022-11-03 13:12:18 -0400

    Solve problem by completing function

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp=(temp-32)*(5/9);
+if(newTemp-newTemp.toFixed(0)!=0){
+  newTemp=Number(newTemp.toFixed(1));
+}
+  return newTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp=(temp*(9/5))+32;
+  if(newTemp-newTemp.toFixed(0)!=0){
+    newTemp=Number(newTemp.toFixed(1));
+  }
+  return newTemp;
 };
 
 // Do not edit below this line
