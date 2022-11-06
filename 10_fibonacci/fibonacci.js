0f7ff1322f2a9d4b4dee4dbb31e1d02a6a5d7f8e
Mayank Kumar <mpareek365@gmail.com>
2022-11-06 04:48:26 -0500

    Solved with time complexity

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let num1 = 0,
+    num2 = 1,
+    num3=1;
+  if (num < 0) return "OOPS";
+  for (let i = 2; i <= num; i++) {
+    num3 = num1 + num2;
+    num1 = num2;
+    num2 = num3;
+  }
+  return num3;
 };
 
 // Do not edit below this line
