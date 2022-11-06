77076e4cd4dc7a135c2b7ad6acd1b9141bac1149
Mayank Kumar <mpareek365@gmail.com>
2022-11-06 04:16:47 -0500

    Created solution by using regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.replace(/[^\w\']/g, "").toLowerCase();
+  let newString = "";
+  for (let i = str.length; i > 0; i--) {
+    newString += str[i - 1];
+  }
+  if (str === newString) return true;
+  else return false;
 };
 
 // Do not edit below this line
