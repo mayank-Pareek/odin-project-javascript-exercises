7448dbf28a3515b7a477afe7737dc1c71e6e0255
Mayank Kumar <mpareek365@gmail.com>
2022-11-06 05:24:39 -0500

    Passed all test

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  let oldest = people[0];
+  for (let i = 0; i < people.length; i++) {
+    if(people[i].yearOfDeath===undefined){
+        people[i].yearOfDeath=2022;
+    }
+    if (
+      people[i].yearOfDeath - people[i].yearOfBirth >
+      oldest.yearOfDeath - oldest.yearOfBirth
+    ) {
+      oldest = people[i];
+    }
+  }
+  return oldest;
 };
 
 // Do not edit below this line
