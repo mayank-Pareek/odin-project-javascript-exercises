0b3eb14cfc2a8060816b04dd30a8d183f829be41
Mayank Kumar <mpareek365@gmail.com>
2022-11-06 18:51:12 -0500

    Passes 3/6 tests using regex

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,8 @@
-const snakeCase = function() {
-
+const snakeCase = function (str) {
+  str = str.replace(/[^\w\s]/g, "").toLowerCase();
+  str = str.replace(/[\s]/g, "_");
+  console.log(str);
+  return str;
 };
 
 // Do not edit below this line
