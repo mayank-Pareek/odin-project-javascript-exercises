0cf91fb8f8cd9d78e6166cfae1e07559fb4113c7
Mayank Kumar <mpareek365@gmail.com>
2022-11-06 16:10:14 -0500

    Solved by splitting string into array

--- pigLatin/pigLatin.js
+++ pigLatin/pigLatin.js
@@ -1,6 +1,23 @@
 function pigLatin(string) {
+  let newString = "";
+  let wordArray = string.split(" ");
 
-};
-  
+  console.log(wordArray);
+  for (let i = 0; i < wordArray.length; i++) {
+    let vowel = wordArray[i].match(/[aeio]/);
+    let index = wordArray[i].indexOf(vowel);
+    if (index > 0) {
+      newString +=
+        wordArray[i].slice(index) + wordArray[i].slice(0, index) + "ay";
+    } else newString += wordArray[i] + "ay";
+
+    if (i != wordArray.length - 1) {
+      newString += " ";
+    }
+  }
+  console.log(newString);
+  return newString;
+}
+
 // Do not edit below this line
 module.exports = pigLatin;
